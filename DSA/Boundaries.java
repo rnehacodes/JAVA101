88eb28b9bf5299c669d6689ffc1a02083fed14c7
Neha Rajput <neharajputcoding@gmail.com>
2024-10-23 23:59:01 +0530

    Logical Operators

--- DSA/Boundaries.java
+++ DSA/Boundaries.java
@@ -3,16 +3,17 @@ import java.util.*;
 public class Boundaries {
     public static void main(String[] args) {
         int[] nums = Randoms.generateAscSortedRandomArray(8, 0, 10);
-        int target = 9, lb = getLowerBound(nums, target);
+        int target = 5, lb = getLowerBound(nums, target), ub = getUpperBound(nums, target);
 
         System.out.println("Lower Bound");
         System.out.print("Array: " + Arrays.toString(nums) + "\n Lower bound of " + target + " found at index: " + lb + " i.e. " + nums[lb]);
 
-        System.out.println("Upper Bound");
-        System.out.print("Array: " + Arrays.toString(nums) + "\n Upper bound of " + target + " is: " + getUpperBound(nums, target));
+        System.out.println("\nUpper Bound");
+        System.out.print("Array: " + Arrays.toString(nums) + "\n Upper bound of " + target + " found at index: " + ub + " i.e. " + nums[lb]);
 
     }
 
+    //Lowerbound -> smallest m such that arr[m] >= target
     //Finding lowerbound (Assuming ascending order sorted array)
     static int getLowerBound(int[] nums, int target) {
         int s = 0, e = nums.length - 1, lb = e;
@@ -29,6 +30,7 @@ public class Boundaries {
         return lb;
     }
 
+    //Upperbound -> smallest m such that arr[m] > target
     //Finding upperbound (Assuming ascending order sorted array)
     static int getUpperBound(int[] nums, int target) {
         int s = 0, e = nums.length - 1, ub = e;
@@ -36,12 +38,11 @@ public class Boundaries {
             int m = (s + e) / 2;
             if(target < nums[m]) {
                 e = m - 1;
-            } else if(target > nums[m]) {
-                s = m + 1;
+                ub = Math.min(ub, m);
             } else {
-                lb = Math.min(lb, m);
+                s = m + 1;
             }
         }
-        return lb;
+        return ub;
     }
 }
--- Dev/Hello.java
+++ Dev/Hello.java
@@ -1,5 +1,6 @@
 class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World, this is not my first java program! :-)");
+        //Logical Operators
     }
 }
